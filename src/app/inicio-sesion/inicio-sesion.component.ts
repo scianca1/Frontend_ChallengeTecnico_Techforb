8fdc40958077ccff047aa72139b0019854d31009
Matias Sciancalepore <msciancalepore47@gmail.com>
2025-01-30 01:32:03 -0300

    agrego funcion para ver contraceña con el ojo

--- src/app/inicio-sesion/inicio-sesion.component.ts
+++ src/app/inicio-sesion/inicio-sesion.component.ts
@@ -1,14 +1,23 @@
-import { CommonModule } from '@angular/common';
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-inicio-sesion',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './inicio-sesion.component.html',
   styleUrl: './inicio-sesion.component.scss'
 })
 export class InicioSesionComponent {
     ver=false;
     value:String="";
+
+    setInputContracenia(){
+     const input = document.getElementById("contracenia");
+     if((input as HTMLInputElement).type==="password"){
+        (input as HTMLInputElement).type = "text"
+     }else{
+      (input as HTMLInputElement).type = "password"
+     }
+    }
 }
